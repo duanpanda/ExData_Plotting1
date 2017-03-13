06db5fe4f033ab9579f1cb8d024b183b23e3cc22
Ryan Duan <duanpanda@gmail.com>
2017-03-12 23:00:32 -0400

    load correct data and draw first plot correctly

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 df <- read.csv("./household_power_consumption.txt",
-               nrows = 2880, skip = 68076, # only read lines for 2-Feb-2007 and 3-Feb-2007
+               nrows = 69518-66637-1, skip = 66636, # only read lines for 1-Feb-2007 and 2-Feb-2007
                sep = ";",
                col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power",
                              "Voltage", "Global_intensity", "Sub_metering_1",
@@ -9,3 +9,6 @@ df <- read.csv("./household_power_consumption.txt",
 t <- paste(df$Date, df$Time) # default sep = " "
 s <- strptime(t, "%d/%m/%Y %H:%M:%S")
 df$Time <- s
+hist(df$Global_active_power, col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
