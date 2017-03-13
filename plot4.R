afab33bda72ea5130c88967f863b0d0e4443048f
Ryan Duan <duanpanda@gmail.com>
2017-03-13 00:21:16 -0400

    finish plot4 but legend size and position is wrong

--- plot4.R
+++ plot4.R
@@ -10,13 +10,28 @@ t <- paste(df$Date, df$Time) # default sep = " "
 s <- strptime(t, "%d/%m/%Y %H:%M:%S")
 df$Time <- s
 
+par(mfrow = c(2, 2))
+# plot 1
+plot(df$Time, df$Global_active_power, type = "l",
+     ylab = "Global Active Power", xlab = "")
+
+# plot 2
+plot(df$Time, df$Voltage, type = "l",
+     xlab = "datetime", ylab = "Voltage")
+
+# plot 3
 plot(df$Time, df$Sub_metering_1, type = "l",
      ylab = "Energy sub metering", xlab = "", col = "black")
 points(df$Time, df$Sub_metering_2, type = "l", col = "red")
 points(df$Time, df$Sub_metering_3, type = "l", col = "blue")
-legend("topright", col = c("black", "red", "blue"),
-       lty = c("solid", "solid", "solid"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.8)
+rect <- legend("topright", col = c("black", "red", "blue"),
+               lty = c("solid", "solid", "solid"),
+               legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n",
+               xjust = 1, y.intersp = 0.4)
 
-dev.copy(png, file = "plot3.png")
+# plot 4
+plot(df$Time, df$Global_reactive_power, type = "l",
+     ylab = "Global_rective_power", xlab = "datetime")
+
+dev.copy(png, file = "plot4.png")
 dev.off()
